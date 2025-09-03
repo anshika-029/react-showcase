30c197b824b5fb7fe9b4df5c1184bafd01c79fdb
anshika-029 <167740156+anshika-029@users.noreply.github.com>
2025-09-03 20:54:01 +0530

    ToDo: Created

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [todo, setTodo] = useState([])
+  const [newTodo, setNewTodo] = useState("");
+  const [isDarkMode, setisDarkMode] = useState(false)
+
+  const toggleDarkMode = () => {
+    setisDarkMode(!isDarkMode)
+
+  }
+
+  useEffect(() => {
+    try {
+      const savedTodos = JSON.parse(localStorage.getItem("todos"));
+      if (Array.isArray(savedTodos)) {
+        setTodo(savedTodos);
+      }
+    } catch (e) {
+      console.error("Invalid todos in localStorage", e);
+      setTodo([]); // reset to empty array if parsing fails
+    }
+  }, []);
+
+
+  // useEffect(() => {
+  //   if (todo.length > 0) {
+  //     localStorage.setItem("todos", JSON.stringify(todo));
+  //   }
+  // }, [todo]);
+
+
+  const handleAdd = () => {
+    if (!newTodo.trim()) return; //prevents empty todos
+    setTodo(td => [...td, { text: newTodo, completed: false }]);
+    setNewTodo(""); // clear input after adding
+  }
+
+  const handleEdit = (index) => {
+    const itemToEdit = todo[index];
+    setNewTodo(itemToEdit.text);
+    setTodo(todo.filter((_, i) => i !== index))
+  }
+
+  const toggleComplete = (index) => {
+    setTodo(prev =>
+      prev.map((t, i) => (i === index ? { ...t, completed: !t.completed } : t))
+    )
+  }
+
+  const handleDelete = (index) => {
+    setTodo(todo.filter((_, i) => i !== index));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`app ${isDarkMode ? 'dark' : 'light'}`}>
+      <div className='wrap'>
+        <h1 className='logo'>ToDo LIST</h1>
+        <div className='add-tasks'>
+          <input
+            id='input'
+            type='text'
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            placeholder='Add your tasks...'
+          />
+          <button className='add' onClick={handleAdd}>SAVE</button>
+          <button className='mode' onClick={toggleDarkMode}>{isDarkMode ? "☀️" : "🌙"}</button>
+        </div>
+      </div>
+
+      <div className='tasks'>
+        <h2>Goals to Achieve</h2>
+        {todo.length === 0 && <div className='notodos'>No To Dos to display.</div>}
+        {todo.map((items, index) => (
+          <div className='list-wrapper' key={index}>
+            <input type='checkbox'
+              checked={items.completed}
+              onChange={() => toggleComplete(index)} />
+            <div className='list'>
+              <span style={{ textDecoration: items.completed ? "line-through" : "none" }}>
+                {items.text}</span>
+              <div>
+                <button onClick={() => handleEdit(index)}>Edit</button>
+                <button onClick={() => handleDelete(index)}>Delete</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
